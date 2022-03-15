3076ece5f399bd01c577faf609da67b0ed6d522d
levan-abramshvili <99519964+levan-abramshvili@users.noreply.github.com>
2022-03-15 14:30:07 +1000

    Add files via upload

--- itprofessions/itprofessions/settings.py
+++ itprofessions/itprofessions/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-isu89_5@4h&up!1%^j$+@#8@((9q2kh(04oe35vx0h8vfkp-i7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
